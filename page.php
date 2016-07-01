8944bb4e712d7a15cdc60d349681a999d495aa49
John Marlett <1fiyero@gmail.com>
2016-07-01 11:01:32 -0500

    added donate page functions

--- page.php
+++ page.php
@@ -1,16 +1,17 @@
 <?php 
+
 $isPipe = $_GET['ajaxpipe'];
 if($isPipe):
 header('Content-Type: application/json');
-endif; ?>
+endif;
 
-<?php $post_hero;
+$post_hero;
 ob_start();
 get_template_part('hero');
 $post_hero = ob_get_contents();
-ob_end_clean(); ?>
+ob_end_clean();
 
-<?php $field = get_field('background_image');
+ $field = get_field('background_image');
 $backgroundimage = $field['url'];
 $title = "";
 $article = "";
@@ -18,42 +19,52 @@ $donate = "";
 if (have_posts()): while (have_posts()) : the_post();
 	$article = do_shortcode(wpautop(get_the_content()));
 	$title = get_the_title();
-endwhile; endif; ?>
+endwhile; endif; 
  
-<?php $content = <<<AI_PAGE
+$content = <<<AI_PAGE
 
 <style type="text/css">
 #contentContainer.parallax-cover { 
 	background-image:url($backgroundimage);
 	background-attachment: fixed;
 	background-position: top center;
-	background-size: cover; }
+	background-size: cover;
+}
 </style>
 
 		<div id="contentContainer" class="clearfix parallax-cover">
-            $post_hero 
+            $post_hero
 			<div class="gridContainer clearfix">
 				<div id="main">
-					<h1>$title</h1>
-					<div> $donate </div>
+					
 					<article>
+					<h1> $title</h1>
 						$article
 					</article>
 				</div>
 			</div>
-        </div>  
-AI_PAGE; ?>
+        </div>
 
-<?php get_footer(); ?> <!-- THis is not working yet!!!-->
+AI_PAGE;
 
-<?php
-    $response = array(
+$response = array(
 	"title" => $title . " | Austin Institute",
 	"url" => get_the_permalink(),
 	"main" => $content,
 	"pageReady" => "\$(\"#m-global-header\").addClass(\"backgroundTransparent\");",
-	"pageLeave" => "\$(\"#m-global-header\").removeClass(\"backgroundTransparent\");" );
+	"pageLeave" => "\$(\"#m-global-header\").removeClass(\"backgroundTransparent\");"
+	);
 
- if (!$isPipe){ get_header(); echo $response["main"]; echo "<script>ai.page.ready=" . json_encode($response["pageReady"]) . ";ai.page.leave=" . json_encode($response["pageLeave"]) . ";</script>"; get_footer();
+if ( !$isPipe ):
 
- } elseif { echo json_encode($response); } ?>
+get_header(); echo $response["main"]; echo "<script>ai.page.ready=" . json_encode($response["pageReady"]) . ";ai.page.leave=" . json_encode($response["pageLeave"]) . ";</script>"; get_footer();
+
+else:
+
+echo json_encode($response);
+
+endif;
+
+ 
+
+?>
